a989f6126e62bd537136219b13558d1152225e57
MartinBriday <briday.martin@gmail.com>
2022-04-09 21:44:52 +0200

    Adding exception for negative price.

---
+++ src/com/intiformation/exception/PrixNegException.java
@@ -0,0 +1,9 @@
+package com.intiformation.exception;
+
+public class PrixNegException extends Exception {
+
+	public PrixNegException() {
+		// TODO Auto-generated constructor stub
+		System.err.println("Valeur de prix négative.");
+	}
+}
